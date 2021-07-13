d04259f99498e11aa4d75ad6293a583bc70ae94c
AgustinReynaud <agustinreynaud6@gmail.com>
2021-07-12 21:05:28 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import "tailwindcss/tailwind.css"
+import Body from './Components/Body/body';
+
+function App() {
+  return (
+    <div className="App">
+    <Body/>
+    </div>
+  );
+}
+
+export default App;
